709701edbb028ec7d6d03e74f5a72dfacee7a9db
thomasliu244 <45839764+thomasliu244@users.noreply.github.com>
2018-12-19 15:23:24 -0500

    Visual changes + side bar content update
    1. Updated side bar
    2. Title bar updated
    3. Got the first image pop up to work, but imcomplete

--- app/client/scripts/imageEnlarge.js
+++ app/client/scripts/imageEnlarge.js
@@ -1,15 +1,3 @@
-"use strict";
-
-(function($){
-
-  $(document).ready(function() {
-    $("#nav-instruction-menu").click(function(){
-      $("#instruction-sidebar").toggleClass("close");
-    });
-  });
-})(jQuery);
-
-
 // Get the modal
 var modal = document.getElementById('myModal');
 
